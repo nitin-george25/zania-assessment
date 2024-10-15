1faac42e11eea13c1705e0a40b0f0daa4f3bbe10
nitin-george25 <nitin.george25@gmail.com>
2024-10-15 11:52:15 +0530

    enabled mock server for prod

--- src/index.tsx
+++ src/index.tsx
@@ -7,9 +7,9 @@ import reportWebVitals from './reportWebVitals';
 import './index.css';
 
 async function enableMocking() {
-  if (process.env.NODE_ENV !== 'development') {
-    return
-  }
+  // if (process.env.NODE_ENV !== 'development') {
+  //   return
+  // }
  
   const { worker } = await import('./mocks/browser')
 
